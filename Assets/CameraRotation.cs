7d6271817d3a5eeebf6aca602eed28ddec911b35
Mathéo Joseph <Matheo.joseph.01@gmail.com>
2025-01-10 22:15:45 -0500

    Player movement fps

---
+++ Assets/CameraRotation.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    
+    public float sensX;
+    public float sensY;
+    
+    public Transform orientation;
+    
+    private float xRotation;
+    private float yRotation;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // rotate cam and orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+
+}
